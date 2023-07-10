4634cf271dc1de5e8238c0258081df8efac63138
shub <shubhampahilwani1@gmail.com>
2023-07-10 12:30:58 +0400

    Solve 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let rev_str = "";
+    for (let i = str.length - 1; i >= 0; i--) rev_str += str[i];
+    return rev_str;
 };
 
 // Do not edit below this line
