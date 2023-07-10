73b1ef984be485a8f79ac623abca68b74800772b
shub <shubhampahilwani1@gmail.com>
2023-07-10 13:44:42 +0400

    Solve 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(far) {
+  return Math.round((5 / 9 * (far - 32)) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  return Math.round((9 / 5 * cel + 32) * 10) / 10;
 };
 
 // Do not edit below this line
