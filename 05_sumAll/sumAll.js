e3d666d68931ec4459d6a0dbb5ea0cb035d91543
shub <shubhampahilwani1@gmail.com>
2023-07-10 13:29:10 +0400

    Solve 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sum = 0;
+    if (!(Number.isInteger(start) && Number.isInteger(end)) || start < 0 || end < 0) return 'ERROR';
+    if (start > end) [start, end] = [end, start];
+    for (let i = start; i <= end; i++) sum += i;
+    return sum;
 };
 
 // Do not edit below this line
