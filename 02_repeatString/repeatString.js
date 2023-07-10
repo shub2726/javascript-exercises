b005a45912b3ef5a127698e903cfaf3d786ea5d0
shub <shubhampahilwani1@gmail.com>
2023-07-10 12:27:36 +0400

    Solve 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(str, times) {
+    if (times < 0) return 'ERROR';
+    let soln = "";
+    for (let i = 0; i < times; i++) soln += str;
+    return soln;
 };
 
 // Do not edit below this line
